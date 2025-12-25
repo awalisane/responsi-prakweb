6c870abf6a1495abc80aa5ba2c5dadbe3da65e8e
awalisane <awalisaniumiyatii@gmail.com>
2025-12-25 14:39:04 +0700

    update 2nd

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 
 # Membuat instance aplikasi Flask
@@ -6,4 +7,5 @@ app = create_app()
 if __name__ == '__main__':
     # Menjalankan aplikasi dalam mode development
     # debug=True memungkinkan auto-reload saat ada perubahan kode
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
